889edb2d432688a4601f24f8008ca7cc772680f6
mateuszpmroz <mateuszpmroz@gmail.com>
2020-04-22 18:23:48 +0200

    handle old functionalities

--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 
 import { images, icons } from 'app/assets';
 import { Image, ScreenTemplate, Header, ListItem } from 'app/components';
+import { AppStorage, Biometric } from 'app/legacy';
 import { en } from 'app/locale';
 
 import { LabeledSettingsRow } from './LabeledSettingsRow';
 
 export const SettingsScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAdvancedOptions, setIsAdvancedOptions] = useState(false);
+  const [biometrics, setBiometrics] = useState({
+    isDeviceBiometricCapable: false,
+    isBiometricsEnabled: false,
+    biometricsType: '',
+  });
+
+  const onAdvancedOptionsChange = async (value: boolean) => {
+    if (value) {
+      await AsyncStorage.setItem(AppStorage.ADVANCED_MODE_ENABLED, '1');
+    } else {
+      await AsyncStorage.removeItem(AppStorage.ADVANCED_MODE_ENABLED);
+    }
+    setIsAdvancedOptions(value);
+  };
+
+  const onFingerprintLoginChange = async (value: boolean) => {
+    if (await Biometric.unlockWithBiometrics()) {
+      biometrics.isBiometricsEnabled = value;
+      await Biometric.setBiometricUseEnabled(value);
+      setBiometrics(biometrics);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      setIsAdvancedOptions(!!(await AsyncStorage.getItem(AppStorage.ADVANCED_MODE_ENABLED)));
+      const isBiometricsEnabled = await Biometric.isBiometricUseEnabled();
+      const isDeviceBiometricCapable = await Biometric.isDeviceBiometricCapable();
+      const biometricsType = (await Biometric.biometricType()) || 'biometrics';
+      setBiometrics({
+        isBiometricsEnabled,
+        isDeviceBiometricCapable,
+        biometricsType,
+      });
+      setIsLoading(false);
+    })();
+  });
+
   const renderGeneralSettings = () => (
     <>
       <ListItem title={en.settings.language} source={icons.languageIcon} />
@@ -16,8 +58,8 @@ export const SettingsScreen = () => {
       <ListItem
         title={en.settings.advancedOptions}
         source={icons.buildIcon}
-        switchValue={false}
-        onSwitchValueChange={() => null}
+        switchValue={isAdvancedOptions}
+        onSwitchValueChange={onAdvancedOptionsChange}
         iconHeight={21}
         iconWidth={20}
       />
@@ -30,8 +72,8 @@ export const SettingsScreen = () => {
       <ListItem
         title={en.settings.fingerprintLogin}
         source={icons.fingerprintIcon}
-        switchValue={true}
-        onSwitchValueChange={() => null}
+        switchValue={biometrics.isBiometricsEnabled}
+        onSwitchValueChange={onFingerprintLoginChange}
         iconWidth={17}
         iconHeight={19}
       />
@@ -40,7 +82,9 @@ export const SettingsScreen = () => {
 
   const renderAboutSettings = () => <ListItem title={en.settings.aboutUs} source={icons.infoIcon} />;
 
-  return (
+  return isLoading ? (
+    <View />
+  ) : (
     <ScreenTemplate>
       <Image source={images.goldWalletLogoBlack} style={styles.logo} resizeMode="contain" />
       <LabeledSettingsRow label={en.settings.general}>{renderGeneralSettings()}</LabeledSettingsRow>
