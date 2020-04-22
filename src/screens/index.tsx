6124f53c250fa6c47612e405412f711867713660
bklukaczewski <bklukaczewski@gmail.com>
2020-04-22 17:00:05 +0200

    Delete contacts with confirmation prompt

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -9,6 +9,7 @@ export { ExportWalletScreen } from './ExportWalletScreen';
 export { DeleteWalletScreen } from './DeleteWalletScreen';
 export { ExportWalletXpubScreen } from './ExportWalletXpubScreen';
 export { default as CreateContactScreen } from './CreateContactScreen';
+export { default as DeleteContactScreen } from './DeleteContactScreen';
 export { default as ContactQRCodeScreen } from './ContactQRCodeScreen';
 export { default as ContactDetailsScreen } from './ContactDetailsScreen';
 export { TransactionDetailsScreen } from './TransactionDetailsScreen';
