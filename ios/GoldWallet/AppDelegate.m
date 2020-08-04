8eca3a6e828cf3a5308418833db986ebfeb19553
mateuszpmroz <mateuszpmroz@gmail.com>
2020-08-04 12:22:32 +0200

    remove ios testing code (for screenshot block)

--- ios/GoldWallet/AppDelegate.m
+++ ios/GoldWallet/AppDelegate.m
@@ -40,32 +40,6 @@
   return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {    
-    // fill screen with our own colour
-    UIView *colourView = [[UIView alloc]initWithFrame:self.window.frame];
-    colourView.backgroundColor = [UIColor whiteColor];
-    colourView.tag = 1234;
-    colourView.alpha = 0;
-    [self.window addSubview:colourView];
-    [self.window bringSubviewToFront:colourView];
-    // fade in the view
-    [UIView animateWithDuration:0.5 animations:^{
-        colourView.alpha = 1;
-    }];
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // grab a reference to our coloured view
-    UIView *colourView = [self.window viewWithTag:1234];
-    // fade away colour view from main view
-    [UIView animateWithDuration:0.5 animations:^{
-        colourView.alpha = 0;
-    } completion:^(BOOL finished) {
-        // remove when finished fading
-        [colourView removeFromSuperview];
-    }];
-}
-
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
   return [RCTLinkingManager application:app openURL:url options:options];
 }
