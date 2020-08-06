20740e2f220873c9d44e9aa3a364fd7a291825fa
KedziaPawel <pawel.kedzia@minebest.com>
2020-08-06 11:06:53 +0200

    Add config for network

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
 const bitcoin = require('bitcoinjs-lib');
+const { Config } = require('react-native-config');
+
+const { SENTRY_DSN, HOST, PORT, BTCV_NETWORK } = Config;
 
 module.exports = {
-  sentryDsn: 'https://dc67fd6d5c2949f2a6853e60eb69d899@o429172.ingest.sentry.io/5375289',
-  network: bitcoin.alt_networks.bitcoinvaultTestnet,
+  sentryDsn: SENTRY_DSN || '',
+  host: HOST || '188.166.204.85',
+  port: PORT || '50001',
+  network: bitcoin.alt_networks[BTCV_NETWORK || 'bitcoinvault'],
 };
