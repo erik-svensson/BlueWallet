f8819bd2271365b67da728d4d83ee11a2d9ed9f3
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-08-07 09:34:47 +0200

    link fix

--- android/app/src/main/java/io/goldwallet/wallet/MainApplication.java
+++ android/app/src/main/java/io/goldwallet/wallet/MainApplication.java
@@ -8,7 +8,6 @@ import com.reactnativerestart.RestartPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 import java.util.List;
 
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new ReactNativeConfigPackage());
       return packages;
     }
 
