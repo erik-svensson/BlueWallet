f45a2cf86ee657440766af3d3b7ba835c1dd9cf1
KedziaPawel <pawel.kedzia@minebest.com>
2020-08-12 13:49:22 +0200

    Fix tests

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   presets: ['@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
   env: {
-    development: {
-      plugins: ['react-native-config-node/transform'],
-    },
-    production: {
+    test: {
       plugins: ['react-native-config-node/transform'],
     },
   },
