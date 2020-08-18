5205776155964e3772ea45cda470d31afcb9b832
KedziaPawel <pawel.kedzia@minebest.com>
2020-08-18 15:27:58 +0200

    Remove unused wallet migration

--- index.js
+++ index.js
@@ -6,52 +6,20 @@ import './shim.js';
 
 import React from 'react';
 import { AppRegistry, StatusBar } from 'react-native';
-import SplashScreen from 'react-native-splash-screen';
 
 import App from './App';
-import WalletMigrate from './walletMigrate';
 
 if (!Error.captureStackTrace) {
   // captureStackTrace is only available when debugging
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   Error.captureStackTrace = () => {};
 }
 
-class BlueAppComponent extends React.Component {
-  state = {
-    isMigratingData: true,
-  };
-
-  componentDidMount() {
-    const walletMigrate = new WalletMigrate(this.setIsMigratingData);
-
-    walletMigrate.start();
-  }
-
-  setIsMigratingData = async () => {
-    SplashScreen.hide();
-    this.setState({
-      isMigratingData: false,
-    });
-  };
-
-  onSuccessfullyAuthenticated = () => {
-    this.setState({
-      successfullyAuthenticated: true,
-    });
-  };
-
-  render() {
-    if (this.state.isMigratingData) {
-      return null;
-    } else {
-      return (
-        <>
-          <StatusBar backgroundColor="rgba(0,0,0,0)" translucent />
-          <App />
-        </>
-      );
-    }
-  }
-}
+const BlueAppComponent = () => (
+  <>
+    <StatusBar backgroundColor="rgba(0,0,0,0)" translucent />
+    <App />
+  </>
+);
 
 AppRegistry.registerComponent('GoldWallet', () => BlueAppComponent);
