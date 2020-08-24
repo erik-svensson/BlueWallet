509caf59a55b31cc11ffcb86b633d8ab5a5e4c50
KedziaPawel <pawel.kedzia@minebest.com>
2020-08-24 09:11:05 +0200

    Improve

--- bla.js
+++ bla.js
@@ -1,6 +1,6 @@
-const { random, range, sortBy } = require('lodash');
+const { random, range, sortBy, findIndex, cloneDeep, orderBy } = require('lodash');
 
-const utxo = [
+const utxoTest = [
   { value: 0.31, id: 4 },
   { value: 2.3, id: 1 },
   { value: 0.00002, id: 2 },
@@ -11,38 +11,94 @@ const utxo = [
   { value: 0.0024324, id: 8 },
 ];
 
-const am = 0.054385;
+const amTest = 0.054385;
 
 const getElementsByIndexs = (arr, indexs) => arr.filter((_, index) => indexs.includes(index));
 
+const removeNotNeededUtxos = ({ utxos, negativeAmount }) => {
+  const start = utxos.length - 1;
+  let nA = negativeAmount;
+  let newUtxos = cloneDeep(utxos);
+
+  for (let i = start; i >= 0; i--) {
+    const utxo = utxos[i];
+    if (utxo.value <= -nA) {
+      nA += utxo.value;
+      newUtxos = [...newUtxos.slice(0, i), ...newUtxos.slice(i + 1)];
+    }
+  }
+  return { utxos: newUtxos, negativeAmount: nA };
+};
+
 const getUtxoWithMinimumRest = (utxos, amount) => {
   const REPS = 100;
+  const sortedUtxos = sortBy(utxos, 'value');
+  const initSolution = { utxos: [], negativeAmount: null };
+
+  let am = amount;
+  for (let i = 0; i < sortedUtxos.length; i++) {
+    const ux = sortedUtxos[i];
+    am -= ux.value;
+
+    initSolution.utxos.push(ux);
+
+    if (am <= 0) {
+      break;
+    }
+  }
+
+  if (am > 0) {
+    return null;
+  }
+
+  initSolution.negativeAmount = am;
+
+  if (initSolution.negativeAmount === 0) {
+    return initSolution;
+  }
+
+  // console.log('initSolution', initSolution);
+  // return removeNotNeededUtxos(initSolution);
+
   const upperTreshold = utxos.length - 1;
-  const solutions = range(REPS).map(() => ({ indexs: [], negativeAmount: null }));
+  const solutions = range(REPS).map(() => ({ utxos: [], negativeAmount: null }));
 
   for (let i = 0; i < REPS; i++) {
     let a = amount;
 
     while (a > 0) {
       const randomIndex = random(0, upperTreshold);
-      if (solutions[i].indexs.includes(randomIndex)) {
+      const u = utxos[randomIndex];
+      if (findIndex(solutions[i].utxos, u) !== -1) {
         continue;
       }
-      a -= utxos[randomIndex].value;
-      solutions[i].indexs.push(randomIndex);
+      a -= u.value;
+      solutions[i].utxos.push(u);
+      if (a > 0) {
+        solutions[i] = removeNotNeededUtxos(solutions[i]);
+      }
     }
     solutions[i].negativeAmount = a;
     if (a === 0) {
-      return getElementsByIndexs(utxos, solutions[i].indexs);
+      return solutions[i].utxos;
     }
   }
-  const [bestSolution] = sortBy(solutions, 'negativeAmount');
+  const [bestSolution] = orderBy(solutions, ['negativeAmount'], ['desc']);
 
-  console.log('solutions', solutions);
-  return getElementsByIndexs(utxos, bestSolution.indexs);
+  return bestSolution.utxos;
 };
 
-const res = getUtxoWithMinimumRest(utxo, am);
+// const res = findIndex(
+//   [
+//     { value: 0.0024324, id: 8 },
+//     { value: 0.042, id: 5 },
+//     { value: 0.00002, id: 2 },
+//     { value: 0.0024324, id: 8 },
+//     { value: 2.3, id: 1 },
+//   ],
+//   { value: 2.3, id: 1 },
+// );
+const res = getUtxoWithMinimumRest(utxoTest, amTest);
 console.log('res', res);
 // const test = (utxs, am) => {
 //   const solutions = [];
