78619c853ecb750c0b22b32eefc6c4e8a3fc00d2
Paweł Kędzia <pawel.kedzia@minebest.com>
2020-09-28 12:04:57 +0200

    Add change spliter

--- BlueElectrum.js
+++ BlueElectrum.js
@@ -29,6 +29,10 @@ async function connectMain() {
       client: '2.7.11',
       version: config.electrumXProtocolVersion,
     });
+    // const relayFee = await mainClient.blockchain_relayfee();
+
+    // const res = (183 * 3 * relayFee) / 1000;
+    // console.log('  const relayFee = await mainClient.blockchain_relayfee()    ', res);
 
     if (ver && ver[0]) {
       console.log('connected to ', ver);
@@ -349,6 +353,12 @@ module.exports.estimateFee = async function(numberOfBlocks) {
       .multipliedBy(100000000)
       .toNumber(),
   );
+};
+
+module.exports.getDustValue = async () => {
+  const relayFee = await mainClient.blockchain_relayfee();
+  // magic numbers from electrum vault
+  return (183 * 3 * relayFee) / 1000;
 };
 
 module.exports.broadcast = async function(hex) {
