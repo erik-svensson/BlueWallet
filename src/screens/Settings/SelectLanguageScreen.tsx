159d77b589d2bc47ab837155405526ceb7fedc81
mateuszpmroz <mateuszpmroz@gmail.com>
2020-10-08 14:54:50 +0200

    detect language in onboarding

--- src/screens/Settings/SelectLanguageScreen.tsx
+++ src/screens/Settings/SelectLanguageScreen.tsx
@@ -1,10 +1,13 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 import { Text, StyleSheet, View, TouchableOpacity, Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { icons } from 'app/assets';
 import { ScreenTemplate, Header, Image } from 'app/components';
 import { Route, MainCardStackNavigatorParams } from 'app/consts';
+import { ApplicationState } from 'app/state';
+import { updateSelectedLanguage } from 'app/state/appSettings/actions';
 import { typography } from 'app/styles';
 
 import { GlobalContext } from '../../../App';
@@ -17,7 +20,7 @@ interface Language {
 }
 
 interface LanguageItemProps {
-  language: Language;
+  selectedLanguage: Language;
   selectedLanguageValue: string;
   onLanguageSelect: (value: string) => void;
 }
@@ -26,12 +29,12 @@ interface SelectLanguageScreenProps {
   navigation: StackNavigationProp<MainCardStackNavigatorParams, Route.SelectLanguage>;
 }
 
-const LanguageItem = ({ language, selectedLanguageValue, onLanguageSelect }: LanguageItemProps) => {
-  const handleLanguageSelect = () => onLanguageSelect(language.value);
+const LanguageItem = ({ selectedLanguage, selectedLanguageValue, onLanguageSelect }: LanguageItemProps) => {
+  const handleLanguageSelect = () => onLanguageSelect(selectedLanguage.value);
   return (
-    <TouchableOpacity key={language.value} onPress={handleLanguageSelect} style={styles.langaugeItemContainer}>
-      <Text style={styles.languageItem}>{language.label}</Text>
-      {language.value === selectedLanguageValue && (
+    <TouchableOpacity key={selectedLanguage.value} onPress={handleLanguageSelect} style={styles.langaugeItemContainer}>
+      <Text style={styles.languageItem}>{selectedLanguage.label}</Text>
+      {selectedLanguage.value === selectedLanguageValue && (
         <View style={styles.successImageContainer}>
           <Image source={icons.success} style={styles.successImage} />
         </View>
@@ -41,16 +44,21 @@ const LanguageItem = ({ language, selectedLanguageValue, onLanguageSelect }: Lan
 };
 
 export const SelectLanguageScreen = (props: SelectLanguageScreenProps) => {
+  const { language } = useSelector((state: ApplicationState) => ({
+    language: state.appSettings.language,
+  }));
+  const dispatch = useDispatch();
+
   const availableLanguages: Language[] = [
     { label: 'English (EN)', value: 'en' },
-    { label: '中文 (ZH)', value: 'zh_cn' },
+    { label: '中文 (ZH)', value: 'zh' },
     { label: 'Español (ES)', value: 'es' },
-    { label: 'Indonesian (ID)', value: 'id_id' },
+    { label: 'Indonesian (ID)', value: 'id' },
     { label: '日本語 (JP)', value: 'ja' },
-    { label: '한국어 (KO)', value: 'ko_kr' },
-    { label: 'Português (PT)', value: 'pt_pt' },
-    { label: 'Tiếng (Việt)', value: 'vi_vn' },
-    { label: 'Türkçe (TR)', value: 'tr_tr' },
+    { label: '한국어 (KO)', value: 'ko' },
+    { label: 'Português (PT)', value: 'pt' },
+    { label: 'Tiếng (Việt)', value: 'vi' },
+    { label: 'Türkçe (TR)', value: 'tr' },
   ];
 
   const onLanguageSelect = (value: string, changeLanguage: Function) => {
@@ -62,6 +70,7 @@ export const SelectLanguageScreen = (props: SelectLanguageScreenProps) => {
           text: i18n.selectLanguage.confirm,
           onPress: async () => {
             await i18n.saveLanguage(value);
+            dispatch(updateSelectedLanguage(value));
             changeLanguage(value);
           },
         },
@@ -80,12 +89,12 @@ export const SelectLanguageScreen = (props: SelectLanguageScreenProps) => {
         <ScreenTemplate
           header={<Header isBackArrow={true} navigation={props.navigation} title={i18n.selectLanguage.header} />}
         >
-          {availableLanguages.map(language => (
+          {availableLanguages.map(item => (
             <LanguageItem
-              language={language}
-              selectedLanguageValue={lang}
+              selectedLanguage={item}
+              selectedLanguageValue={language}
               onLanguageSelect={(val: string) => onLanguageSelect(val, changeLanguage)}
-              key={language.value}
+              key={item.value}
             />
           ))}
         </ScreenTemplate>
--- src/state/appSettings/reducer.ts
+++ src/state/appSettings/reducer.ts
@@ -1,13 +1,17 @@
+import { CONST } from 'app/consts';
+
 import { AppSettingsAction, AppSettingsActionType } from './actions';
 
 export interface AppSettingsState {
   isBiometricsEnabled: boolean;
   isAdvancedOptionsEnabled: boolean;
+  language: string;
 }
 
 const initialState: AppSettingsState = {
   isBiometricsEnabled: false,
   isAdvancedOptionsEnabled: false,
+  language: CONST.defaultLanguage,
 };
 
 export const appSettingsReducer = (state = initialState, action: AppSettingsActionType): AppSettingsState => {
@@ -21,6 +25,11 @@ export const appSettingsReducer = (state = initialState, action: AppSettingsActi
       return {
         ...state,
         isAdvancedOptionsEnabled: action.value,
+      };
+    case AppSettingsAction.UpdateSelectedLanguage:
+      return {
+        ...state,
+        language: action.value,
       };
     default:
       return state;
