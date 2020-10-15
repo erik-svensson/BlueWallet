df7082630127262bb2a9ea6c823138ae2a293ef0
Sebastian Antolik <sebastian.antolik@minebest.com>
2020-10-15 14:02:33 +0200

    missing loc

--- scripts/missing-translations/zh_to-improve.js
+++ scripts/missing-translations/zh_to-improve.js
@@ -229,7 +229,7 @@ const zh = {
       receiveWithAmount: '接收金额',
       share: '分享',
       shareWalletAddress: 'TRANSLATION NEEDED | ENG: Share wallet address',
-      receiveWithAmountSubtitle: 'TRANSLATION NEEDED | ENG: Enter the amount which you would like to receive. The entered amount will be included in the above QR code',
+      receiveWithAmountSubtitle: 'TRANSLATION NEEDED | ENG: Enter the amount which you would like to receive. The QR code will update accordingly to include the amount.',
     },
     header: '接收币',
     label: 'TRANSLATION NEEDED | ENG: Wallet address',
