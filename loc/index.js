88baf8e7bb45b237f56d2608d70c7c5b8f106997
mateuszpmroz <mateuszpmroz@gmail.com>
2020-10-23 14:52:07 +0200

    fix date translations for english language on switch

--- loc/index.js
+++ loc/index.js
@@ -3,11 +3,17 @@ import dayjs from 'dayjs';
 import localeData from 'dayjs/plugin/localeData';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import Localization from 'react-localization';
-import { LocaleConfig } from 'react-native-calendars';
+import {
+  LocaleConfig
+} from 'react-native-calendars';
 import * as RNLocalize from 'react-native-localize';
 
-import { BitcoinUnit } from '../models/bitcoinUnits';
-import { CONST } from '../src/consts';
+import {
+  BitcoinUnit
+} from '../models/bitcoinUnits';
+import {
+  CONST
+} from '../src/consts';
 
 const BigNumber = require('bignumber.js');
 
@@ -29,7 +35,6 @@ dayjs.extend(localeData);
 const init = async lang => {
   if (lang) {
     strings.setLanguage(lang);
-    let localeForDayJSAvailable = true;
     switch (lang) {
       case 'zh':
         require('dayjs/locale/zh-cn');
@@ -57,12 +62,9 @@ const init = async lang => {
         break;
       default:
         lang = CONST.defaultLanguage;
-        localeForDayJSAvailable = false;
         break;
     }
-    if (localeForDayJSAvailable) {
-      dayjs.locale(lang.split('_')[0]);
-    }
+    dayjs.locale(lang);
     await AsyncStorage.setItem('lang', lang);
     LocaleConfig.locales[lang] = strings.getListOfMonthsAndWeekdays();
     LocaleConfig.defaultLocale = lang;
