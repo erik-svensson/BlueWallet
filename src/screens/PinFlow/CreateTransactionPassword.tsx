45e350a0e1276b02e6fe237867330555f36349e4
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-10-28 14:35:23 +0100

    v5.0.11

--- src/screens/PinFlow/CreateTransactionPassword.tsx
+++ src/screens/PinFlow/CreateTransactionPassword.tsx
@@ -113,7 +113,7 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     width: '100%',
   },
-  visibilityIcon: { position: 'absolute', right: 0, bottom: 30, zIndex: 3, borderWidth: 1 },
+  visibilityIcon: { position: 'absolute', right: 0, bottom: 30, zIndex: 3 },
   icon: {
     width: 24,
     height: 24,
