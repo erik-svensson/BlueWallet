e6e40149f9511c6c849991027a99f2f3e2752d09
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-11-06 11:19:19 +0100

    navigate to error screen on wrong key

--- src/consts/models.tsx
+++ src/consts/models.tsx
@@ -353,12 +353,14 @@ export type MainCardStackNavigatorParams = {
   [Route.ContactQRCode]: { contact: Contact };
   [Route.TransactionDetails]: { transaction: EnhancedTransaction };
   [Route.ReceiveCoins]: { id: string };
-  [Route.SendCoins]: {
-    fromSecret?: string;
-    fromAddress?: string;
-    fromWallet?: Wallet;
-    toAddress?: string;
-  };
+  [Route.SendCoins]:
+    | {
+        fromSecret?: string;
+        fromAddress?: string;
+        fromWallet?: Wallet;
+        toAddress?: string;
+      }
+    | undefined;
   [Route.SendCoinsConfirm]: {
     fee: number;
     feeSatoshi?: number;
