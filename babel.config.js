46ec5ee2a17f20f869fe867955d7e36408931a4c
Mateusz Mróz <mateuszpmroz@gmail.com>
2020-11-06 15:49:49 +0100

    remove package json, add config in babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,18 @@ module.exports = {
   presets: ['@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
   env: {
     test: {
-      plugins: ['react-native-config-node/transform'],
+      plugins: [
+        'react-native-config-node/transform',
+        [
+          'module-resolver',
+          {
+            root: ['./src'],
+            alias: {
+              app: './src',
+            },
+          },
+        ],
+      ],
     },
   },
 };
