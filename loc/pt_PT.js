794f477cb3bf587c72901aa2cfcbbf2ec7a2ddba
Sebastian Antolik <sebastian.antolik@minebest.com>
2020-11-18 13:34:46 +0100

    up

--- loc/pt_PT.js
+++ loc/pt_PT.js
@@ -374,7 +374,7 @@ module.exports = {
       type: 'Tipo de transação',
       watchOnlyError: 'Carteiras apenas de observação não podem enviar transações',
     },
-    warning: 'Aviso:',
+    warning: 'Aviso: ',
     warningGeneral:
       'Esteja ciente de que no processo de uso do recurso Transação segura, uma parte dos fundos deixados na sua carteira pode ser bloqueada. Este é um procedimento normal vinculado ao UTXO e aos parâmetros de blockchain da carteira Bitcoin Vault. Os seus fundos serão desbloqueados assim que a transação for verificada (após aproximadamente 24 horas) ou cancelada (dentro de 24 horas).',
   },
