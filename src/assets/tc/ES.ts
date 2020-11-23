6f489410e56fcdf1744d408fe972786781ba502c
Sebastian Antolik <sebastian.antolik@minebest.com>
2020-11-23 16:05:45 +0100

    up comma

--- src/assets/tc/ES.ts
+++ src/assets/tc/ES.ts
@@ -11,7 +11,7 @@ export const es = `<!DOCTYPE html>
             font-weight: 300;
             text-align: justify;
             font-size: 15px
-        },
+        }
         h3 {
             font-family: 'Ubuntu';
             font-weight: 700;
