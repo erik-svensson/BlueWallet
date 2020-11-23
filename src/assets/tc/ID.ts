9c643184b9f777af8ef999765ab1516f9fe2dc07
Sebastian Antolik <sebastian.antolik@minebest.com>
2020-11-23 13:23:31 +0100

    up

--- src/assets/tc/ID.ts
+++ src/assets/tc/ID.ts
@@ -3,9 +3,17 @@ export const id = `<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,700;1,300&display=swap" rel="stylesheet">
     <style>
         p {
+            font-family: 'Ubuntu';
+            font-weight: 300;
             text-align: justify;
+        },
+        h3 {
+            font-family: 'Ubuntu';
+            font-weight: 700;
         }
     </style>
 </head>
