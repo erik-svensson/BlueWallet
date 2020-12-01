d4e1641e2127ce48fe84f850b3bebe625f2bdc2a
Paweł Kędzia <pawel.kedzia@minebest.com>
2020-12-01 13:07:47 +0100

    Fix tests

--- tests/integration/App.test.js
+++ tests/integration/App.test.js
@@ -2,39 +2,28 @@
 
 import { LegacyWallet, SegwitP2SHWallet } from '../../class';
 
-global.net = require('net');
 global.crypto = require('crypto'); // shall be used by tests under nodejs CLI, but not in RN environment
 const assert = require('assert');
 
-const BlueElectrum = require('../../BlueElectrum');
+global.net = require('net'); // needed by Electrum client. For RN it is proviced in shim.js
+const BlueElectrum = require('../../BlueElectrum'); // so it connects ASAP
 
-jest.setTimeout(300000);
-
-jest.mock('react-native-qrcode-svg', () => 'Video');
-jest.useFakeTimers();
-
-jest.mock('amplitude-js', () => ({
-  getInstance() {
-    return {
-      init: jest.fn(),
-      logEvent: jest.fn(),
-    };
-  },
-}));
+afterAll(async () => {
+  // after all tests we close socket so the test suite can actually terminate
+  BlueElectrum.forceDisconnect();
+  return new Promise(resolve => setTimeout(resolve, 10000)); // simple sleep to wait for all timeouts termination
+});
 
 beforeAll(async () => {
   // awaiting for Electrum to be connected. For RN Electrum would naturally connect
   // while app starts up, but for tests we need to wait for it
-  try {
-    await BlueElectrum.waitTillConnected();
-  } catch (Err) {
-    console.log('failed to connect to Electrum:', Err);
-    process.exit(2);
-  }
+  await BlueElectrum.waitTillConnected();
 });
 
 describe('unit - LegacyWallet', function() {
   it('serialize and unserialize work correctly', () => {
+    jest.setTimeout(30000);
+
     const a = new LegacyWallet();
     +a.fetchBalance(); // to initalize BlueElectrum, to be replaced with something better
     a.setLabel('my1');
@@ -47,6 +36,7 @@ describe('unit - LegacyWallet', function() {
   });
 
   it('can validate addresses', () => {
+    jest.setTimeout(30000);
     const w = new LegacyWallet();
     assert.ok(w.isAddressValid('YRMDysNqxPQiHee3NodziKKsHhRvysur63'));
     assert.ok(!w.isAddressValid('YRMDysNqxPQiHee3NodziKKsHhRvysur64'));
@@ -58,7 +48,10 @@ describe('unit - LegacyWallet', function() {
 });
 
 it('SegwitP2SHWallet can generate segwit P2SH address from WIF', async () => {
+  jest.setTimeout(30000);
+
   const l = new SegwitP2SHWallet();
+
   l.setSecret('Kxr9tQED9H44gCmp6HAdmemAzU3n84H3dGkuWTKvE23JgHMW8gct');
   assert.strictEqual(l.getAddress(), 'RBkrVH6nanQxjQ6n99nPHXcvY73u3jBLdU');
   assert.strictEqual(l.getAddress(), await l.getAddressAsync());
@@ -86,6 +79,8 @@ it('Wallet can fetch UTXO', async () => {
 });
 
 xit('Wallet can fetch TXs', async () => {
+  jest.setTimeout(30000);
+
   const w = new LegacyWallet();
   w._address = 'YRMrqNUKAfA2bQ7RmSz1hLYCeGAtci8NkT';
   await w.fetchTransactions();
