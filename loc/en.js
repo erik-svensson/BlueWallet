c385d3a4109f8611c89b430763e0d6a254c788c8
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-12-10 09:27:14 +0100

    add email

--- loc/en.js
+++ loc/en.js
@@ -292,9 +292,19 @@ module.exports = {
     noteSecond: 'If you want to change your email address you can do it later in Settings.',
   },
   notifications: {
+    addYourAddress: 'Add your email address',
+    addYourEmailFor: 'Add your email for notifications',
+    addEmail: 'Add email',
+    addYourAddressDescription: 'It is used to send you transaction notifications from chosen wallets.',
+    addYourEmailForDescription:
+      'You can add your email address. It is used to send you transaction notifications from chosen wallets.',
+    change: 'Change your email',
     title: 'Configure your notifications',
+    yourEmail: 'Your email',
     description:
       'You can change your email address. It is used to send you transaction notifications from chosen wallets.',
+    delete: 'Delete email',
+    yourSubscriptions: 'Your subscriptions',
   },
   onboarding: {
     changePin: 'Change PIN',
