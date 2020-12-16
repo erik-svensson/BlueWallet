25e1cf7899b28b758288584c2787ff5b3fbcf70d
Sebastian Antolik <sebastian.antolik@minebest.com>
2020-12-16 12:43:06 +0100

    up

---
+++ src/api/types.ts
@@ -0,0 +1,50 @@
+import { Transaction } from 'app/consts';
+
+export interface Wallet {
+  balance: number;
+  hideBalance: boolean;
+  preferredBalanceUnit: string;
+  label: string;
+  chain: string;
+  num_addresses: number;
+  transactions: Transaction[];
+  address?: string;
+  secret: string;
+  type: string;
+  typeReadable: string;
+  unconfirmed_balance: number;
+  confirmed_balance: number;
+  outgoing_balance: number;
+  incoming_balance: number;
+  utxo: any[];
+  _xpub: string;
+  id: string;
+  password?: string;
+}
+
+export interface SubscribePayload {
+  wallets: Wallet[];
+  mail: string;
+  lang: string;
+}
+
+export interface UnsubscribePayload {
+  hashes: string[];
+  mail: string;
+}
+
+export interface AuthenticatePayload {
+  session_token: string;
+  pin: number;
+}
+
+export interface ModifyPayload {
+  hashes: string[];
+  old_mail: string;
+  new_mail: string;
+}
+
+export interface CheckSubscriptionPayload {
+  hashes: string[];
+  mail: string;
+}
