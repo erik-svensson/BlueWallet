7e8935f5afb69ee7310c31f18fac23b5992171cf
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-12-23 14:40:42 +0100

    tmp

--- src/state/notifications/sagas.ts
+++ src/state/notifications/sagas.ts
@@ -10,6 +10,7 @@ import {
 export function* createNotificationEmailSaga(action: CreateNotificationEmailAction | unknown) {
   const { meta, payload } = action as CreateNotificationEmailAction;
   try {
+    // yield call(StoreService.setStoreValue, 'email', payload.email);
     yield put(createNotificationEmailSuccess(payload.email));
     if (meta?.onSuccess) {
       meta.onSuccess();
