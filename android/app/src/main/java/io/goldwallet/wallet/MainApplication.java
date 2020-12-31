a352b329864829f9ea958eecd8a6924cd5cb21f8
Marek Siemieniuk-Morawski <43886953+marek-siemieniuk-morawski@users.noreply.github.com>
2020-12-31 14:01:39 +0100

    Removed duplicated RNSentry module declaration (#561)

--- android/app/src/main/java/io/goldwallet/wallet/MainApplication.java
+++ android/app/src/main/java/io/goldwallet/wallet/MainApplication.java
@@ -11,8 +11,6 @@ import com.facebook.soloader.SoLoader;
 import io.goldwallet.PreventScreenshotPackage;
 import java.util.List;
 
-import io.sentry.react.RNSentryPackage;
-
 public class MainApplication extends Application implements ReactApplication {  
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -27,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
       packages.add(new PreventScreenshotPackage());
-      packages.add(new RNSentryPackage());
       
       return packages;
     }
