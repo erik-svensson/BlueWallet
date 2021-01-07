0c31f24214496771289f255874b7197f4c11bf00
Sebastian Antolik <sebastian.antolik@minebest.com>
2021-01-07 11:18:15 +0100

    up

--- src/helpers/decode.ts
+++ src/helpers/decode.ts
@@ -10,15 +10,15 @@ import CryptoJS from 'crypto-js';
  */
 
 export const decryptCode = (email: string, pin: string) => {
-  const sliceNumber = 32;
+  const cipherTextHalfLentgh = 32;
 
   const secret = CryptoJS.enc.Hex.parse(CryptoJS.SHA256(email).toString());
 
   const unBase64CipherText = CryptoJS.enc.Base64.parse(pin);
 
-  const extractedCipherText = unBase64CipherText.toString().slice(-sliceNumber);
+  const extractedCipherText = unBase64CipherText.toString().slice(-cipherTextHalfLentgh);
 
-  const extractedIv = unBase64CipherText.toString().slice(0, sliceNumber);
+  const extractedIv = unBase64CipherText.toString().slice(0, cipherTextHalfLentgh);
 
   const decrypted = CryptoJS.AES.decrypt(extractedCipherText, secret, {
     iv: CryptoJS.enc.Hex.parse(extractedIv),
