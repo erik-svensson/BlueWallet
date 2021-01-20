5f4fe450408d9ab70895db5e077255001d5eeee1
marek-siemieniuk-morawski <marek.siemieniuk-morawski@outlook.com>
2021-01-20 16:45:13 +0100

    WIP

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,39 +1,18 @@
 import { alt_networks, Network } from 'bitcoinjs-lib';
 import Config from 'react-native-config';
 
-const {
-  ENVIRONMENT,
-  SENTRY_DSN,
-  HOSTS,
-  PORT,
-  BTCV_NETWORK,
-  PROTOCOL,
-  ELECTRUM_X_PROTOCOL_VERSION,
-  IS_BETA,
-  APP_ID,
-  APPLICATION_NAME,
-  EXPLORER_URL,
-  NOTIFICATION_EMAIL_URL,
-} = Config;
-
-const defaultNetworkName = 'bitcoinvault';
-
-const networkName = BTCV_NETWORK || defaultNetworkName;
-
-const hosts = (HOSTS || 'electrumx-mainnet1.bitcoinvault.global,electrumx-mainnet2.bitcoinvault.global').split(',');
-
 export default {
-  environment: ENVIRONMENT,
-  isBeta: IS_BETA === 'true',
-  applicationId: APP_ID,
-  applicationName: APPLICATION_NAME,
-  hosts,
-  port: PORT || '443',
-  networkName,
-  network: alt_networks[networkName] as Network,
-  protocol: PROTOCOL || 'tcp',
-  electrumXProtocolVersion: ELECTRUM_X_PROTOCOL_VERSION || '2.0',
-  explorerUrl: EXPLORER_URL || 'https://explorer.bitcoinvault.global',
-  sentryDsn: SENTRY_DSN,
-  emailNotificationsURL: NOTIFICATION_EMAIL_URL,
+  environment: Config.ENVIRONMENT,
+  isBeta: Config.IS_BETA === 'true',
+  applicationId: Config.APP_ID,
+  applicationName: Config.APPLICATION_NAME,
+  hosts: Config.HOSTS.split(','),
+  port: Config.PORT,
+  networkName: Config.BTCV_NETWORK,
+  network: alt_networks[Config.BTCV_NETWORK] as Network,
+  protocol: Config.PROTOCOL,
+  electrumXProtocolVersion: Config.ELECTRUM_X_PROTOCOL_VERSION,
+  explorerUrl: Config.EXPLORER_URL,
+  sentryDsn: Config.SENTRY_DSN,
+  emailNotificationsURL: Config.NOTIFICATION_EMAIL_URL,
 };
