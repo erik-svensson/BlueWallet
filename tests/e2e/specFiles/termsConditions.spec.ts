c92ba645a3eb0734905ed6b93d1d157640866f01
Marek Siemieniuk-Morawski <43886953+marek-siemieniuk-morawski@users.noreply.github.com>
2021-01-22 15:09:45 +0100

    Fixed an import flow when email address is not configured yet (#589)

--- tests/e2e/specFiles/termsConditions.spec.ts
+++ tests/e2e/specFiles/termsConditions.spec.ts
@@ -1,7 +1,6 @@
-import { expect, waitFor } from 'detox';
-
+import { wait } from '../../../utils/time';
 import { expectToBeDisabled } from '../assertions';
-import { isBeta } from '../helpers';
+import { isBeta, SECOND } from '../helpers';
 import app from '../pageObjects';
 
 describe('Terms & Conditions', () => {
@@ -10,6 +9,9 @@ describe('Terms & Conditions', () => {
       isBeta() && (await app.onboarding.betaVersionScreen.close());
 
       await app.developerRoom.tapOnDoNothingButton();
+
+      // TODO: I am ashamed of it :(
+      await wait(10 * SECOND);
       await app.termsConditionsScreen.scrollDown();
       await app.termsConditionsScreen.tapOnAgreementCheckboxes();
       await app.termsConditionsScreen.tapOnAgreeButton();
