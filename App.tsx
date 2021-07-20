a838cc18bf43b190ea9970222708c60d963c7414
mlcbest <mykhailo.lobaievskyi@cloudbest.com>
2021-07-20 13:24:16 +0200

    Code push fixed, added codepush update label for sentry

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import * as Sentry from '@sentry/react-native';
-import React from 'react';
+import React, { Component } from 'react';
 import { I18nextProvider } from 'react-i18next';
-import { View, StyleSheet, LogBox } from 'react-native';
+import { View, StyleSheet, LogBox, Platform } from 'react-native';
 import codePush from 'react-native-code-push';
+import Config from 'react-native-config';
+import VersionNumber from 'react-native-version-number';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
@@ -19,22 +21,47 @@ const i18n = require('./loc');
 
 LogBox.ignoreAllLogs(process.env.LOG_BOX_IGNORE === 'true');
 
-if (!__DEV__) {
-  Sentry.init({
-    dsn: config.sentryDsn,
-    tracesSampleRate: 0.25,
-    debug: config.environment === 'dev',
-    environment: config.environment,
-  });
-}
+const sentryOptions = {
+  dsn: config.sentryDsn,
+  tracesSampleRate: 0.25,
+  debug: config.environment === 'dev',
+  environment: config.environment,
+};
 
 const getNewKey = () => new Date().toISOString();
 
 const codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  installMode: codePush.InstallMode.ON_NEXT_RESUME,
+  installMode: codePush.InstallMode.IMMEDIATE,
   minimumBackgroundDuration: 30 * 60, // 30 minutes
+  updateDialog: true,
+  // TODO: Fix deployment keys for ios
+  deploymentKey: Platform.OS === 'ios' ? Config.CODEPUSH_DEPLOYMENT_KEY_IOS : Config.CODEPUSH_DEPLOYMENT_KEY_ANDROID,
 };
+
+if (!__DEV__) {
+  Sentry.init(sentryOptions);
+
+  if (VersionNumber.bundleIdentifier && VersionNumber.appVersion && VersionNumber.buildVersion) {
+    Sentry.setRelease(`${VersionNumber.bundleIdentifier}@${VersionNumber.appVersion}+${VersionNumber.buildVersion}`);
+  }
+
+  codePush.getUpdateMetadata().then(update => {
+    // Init sentry after update check finished
+    if (update) {
+      // Set release depends on codepush update
+      Sentry.setRelease(update.appVersion + '-codepush:' + update.label);
+    }
+  });
+}
+
+class CodePushClass extends Component<null, null> {
+  render() {
+    return null;
+  }
+}
+
+const WithCodePush = codePush(codePushOptions)(CodePushClass);
 
 class App extends React.PureComponent {
   state = {
@@ -59,25 +86,28 @@ class App extends React.PureComponent {
 
   render() {
     return (
-      <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
-          <AppStateManager
-            handleAppComesToForeground={this.setUnlockScreenKey}
-            handleAppComesToBackground={this.lockScreen}
-          />
-          <PersistGate loading={null} persistor={persistor}>
-            <View style={styles.wrapper}>
-              <Navigator unlockKey={this.state.unlockKey} />
-            </View>
-            <NotificationsServices />
-          </PersistGate>
-        </Provider>
-      </I18nextProvider>
+      <>
+        {!__DEV__ && <WithCodePush />}
+        <I18nextProvider i18n={i18n}>
+          <Provider store={store}>
+            <AppStateManager
+              handleAppComesToForeground={this.setUnlockScreenKey}
+              handleAppComesToBackground={this.lockScreen}
+            />
+            <PersistGate loading={null} persistor={persistor}>
+              <View style={styles.wrapper}>
+                <Navigator unlockKey={this.state.unlockKey} />
+              </View>
+              <NotificationsServices />
+            </PersistGate>
+          </Provider>
+        </I18nextProvider>
+      </>
     );
   }
 }
 
-export default codePush(codePushOptions)(App);
+export default App;
 
 const styles = StyleSheet.create({
   wrapper: {
