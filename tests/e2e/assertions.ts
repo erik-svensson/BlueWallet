4cc916e8f7a051114c0d5b8bcacfceffe225b230
Dawid Gąbka <dawid.gabka@cloudbest.com>
2021-07-21 09:37:59 +0200

    add comment to fix expectToBeDisabled function

--- tests/e2e/assertions.ts
+++ tests/e2e/assertions.ts
@@ -1,5 +1,9 @@
 import Detox, { expect } from 'detox';
 
+// FIX: This function does not do what it's supposed to
+// when tapping the button
+// if error is thrown - nothing happens
+// if no error is thrown - nothing happens
 export const expectToBeDisabled = async (element: Detox.IndexableNativeElement): Promise<void> => {
   try {
     await expect(element).toBeVisible();
