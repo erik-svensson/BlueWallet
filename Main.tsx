968789850d8df735134d8c3bd117b73076f72768
mlcbest <mykhailo.lobaievskyi@cloudbest.com>
2021-08-02 15:57:22 +0200

    Show spalash screen till app will be loaded

--- Main.tsx
+++ Main.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 
 import App from './App';
 
 const Main = () => {
+  useEffect(() => {
+    RNBootSplash.show();
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor="rgba(0,0,0,0)" translucent />
